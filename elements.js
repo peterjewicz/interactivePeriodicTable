bcd1cc6a5cdeb9a1caafee9197542a3aa419beb5
peterjewicz <peterjewicz@hotmail.com>
2016-03-11 22:36:03 -0600

    initial commit

---
+++ elements.js
@@ -0,0 +1,355 @@
+//All info here
+
+
+var elements = {}
+
+elements[1] = {};
+elements[1]['number'] = 1;
+elements[1]['symbol'] = "H";
+elements[1]['name'] = "Hydrogen";
+elements[1]['weight'] = 1.008;
+
+elements[2] = {};
+elements[2]['number'] = 2;
+elements[2]['symbol'] = "He";
+elements[2]['name'] = "Helium";
+elements[2]['weight'] = 4.002;
+
+elements[3] = {};
+elements[3]['number'] = 3;
+elements[3]['symbol'] = "Li";
+elements[3]['name'] = "Lithium";
+elements[3]['weight'] = 6.94;
+
+elements[4] = {};
+elements[4]['number'] = 4;
+elements[4]['symbol'] = "Be";
+elements[4]['name'] = "Beryllium";
+elements[4]['weight'] = 9.012;
+
+elements[5] = {};
+elements[5]['number'] = 5;
+elements[5]['symbol'] = "B";
+elements[5]['name'] = "Boron";
+elements[5]['weight'] = 10.811;
+
+elements[6] = {};
+elements[6]['number'] = 6;
+elements[6]['symbol'] = "C";
+elements[6]['name'] = "Carbon";
+elements[6]['weight'] = 12.01;
+
+elements[7] = {};
+elements[7]['number'] = 7;
+elements[7]['symbol'] = "N";
+elements[7]['name'] = "Nitrogen";
+elements[7]['weight'] = 14.006;
+
+elements[8] = {};
+elements[8]['number'] = 8;
+elements[8]['symbol'] = "O";
+elements[8]['name'] = "Oxygen";
+elements[8]['weight'] = 15.999;
+
+elements[9] = {};
+elements[9]['number'] = 9;
+elements[9]['symbol'] = "F";
+elements[9]['name'] = "Flourine";
+elements[9]['weight'] = 18.998;
+
+elements[10] = {};
+elements[10]['number'] = 10;
+elements[10]['symbol'] = "Ne";
+elements[10]['name'] = "Neon";
+elements[10]['weight'] = 20.179;
+
+elements[11] = {};
+elements[11]['number'] = 11;
+elements[11]['symbol'] = "Na";
+elements[11]['name'] = "Sodium";
+elements[11]['weight'] = 22.989;
+
+elements[12] = {};
+elements[12]['number'] = 12;
+elements[12]['symbol'] = "Mg";
+elements[12]['name'] = "Magnesium";
+elements[12]['weight'] = 24.305;
+
+elements[13] = {};
+elements[13]['number'] = 13;
+elements[13]['symbol'] = "Al";
+elements[13]['name'] = "Aluminium";
+elements[13]['weight'] = 26.981;
+
+elements[14] = {};
+elements[14]['number'] = 14;
+elements[14]['symbol'] = "Si";
+elements[14]['name'] = "Silicon";
+elements[14]['weight'] = 28.085;
+
+elements[15] = {};
+elements[15]['number'] = 15;
+elements[15]['symbol'] = "P";
+elements[15]['name'] = "Phosphorus";
+elements[15]['weight'] = 30.973;
+
+elements[16] = {};
+elements[16]['number'] = 16;
+elements[16]['symbol'] = "S";
+elements[16]['name'] = "Sulfur";
+elements[16]['weight'] = 32.065;
+
+elements[17] = {};
+elements[17]['number'] = 17;
+elements[17]['symbol'] = "Cl";
+elements[17]['name'] = "Chlorine";
+elements[17]['weight'] = 35.453;
+
+elements[18] = {};
+elements[18]['number'] = 18;
+elements[18]['symbol'] = "Ar";
+elements[18]['name'] = "Argon";
+elements[18]['weight'] = 39.948;
+
+elements[19] = {};
+elements[19]['number'] = 19;
+elements[19]['symbol'] = "K";
+elements[19]['name'] = "Potassium";
+elements[19]['weight'] = 39.098;
+
+elements[20] = {};
+elements[20]['number'] = 20;
+elements[20]['symbol'] = "Ca";
+elements[20]['name'] = "Calcium";
+elements[20]['weight'] = 40.078;
+
+elements[21] = {};
+elements[21]['number'] = 21;
+elements[21]['symbol'] = "Sc";
+elements[21]['name'] = "Scandium";
+elements[21]['weight'] = 44.955;
+
+elements[22] = {};
+elements[22]['number'] = 22;
+elements[22]['symbol'] = "Ti";
+elements[22]['name'] = "Titanium";
+elements[22]['weight'] = 47.867;
+
+elements[23] = {};
+elements[23]['number'] = 23;
+elements[23]['symbol'] = "V";
+elements[23]['name'] = "Vanadium";
+elements[23]['weight'] = 50.941;
+
+elements[24] = {};
+elements[24]['number'] = 24;
+elements[24]['symbol'] = "Cr";
+elements[24]['name'] = "Chromium";
+elements[24]['weight'] = 51.996;
+
+elements[25] = {};
+elements[25]['number'] = 25;
+elements[25]['symbol'] = "Mn";
+elements[25]['name'] = "Manganese";
+elements[25]['weight'] = 54.938;
+
+elements[26] = {};
+elements[26]['number'] = 26;
+elements[26]['symbol'] = "Fe";
+elements[26]['name'] = "Iron";
+elements[26]['weight'] = 55.845;
+
+elements[27] = {};
+elements[27]['number'] = 27;
+elements[27]['symbol'] = "Co";
+elements[27]['name'] = "Cobalt";
+elements[27]['weight'] = 58.933;
+
+elements[28] = {};
+elements[28]['number'] = 28;
+elements[28]['symbol'] = "Ni";
+elements[28]['name'] = "Nickel";
+elements[28]['weight'] = 58.693;
+
+elements[29] = {};
+elements[29]['number'] = 29;
+elements[29]['symbol'] = "Cu";
+elements[29]['name'] = "Copper";
+elements[29]['weight'] = 63.546;
+
+elements[30] = {};
+elements[30]['number'] = 30;
+elements[30]['symbol'] = "Zn";
+elements[30]['name'] = "Zinc";
+elements[30]['weight'] = 65.38;
+
+elements[31] = {};
+elements[31]['number'] = 31;
+elements[31]['symbol'] = "Ga";
+elements[31]['name'] = "Gallium";
+elements[31]['weight'] = 69.723;
+
+elements[32] = {};
+elements[32]['number'] = 32;
+elements[32]['symbol'] = "Ge";
+elements[32]['name'] = "Germanium";
+elements[32]['weight'] = 72.64;
+
+elements[33] = {};
+elements[33]['number'] = 33;
+elements[33]['symbol'] = "As";
+elements[33]['name'] = "Arsenic";
+elements[33]['weight'] = 74.921;
+
+elements[34] = {};
+elements[34]['number'] = 34;
+elements[34]['symbol'] = "Se";
+elements[34]['name'] = "Selenium";
+elements[34]['weight'] = 78.96;
+
+elements[35] = {};
+elements[35]['number'] = 35;
+elements[35]['symbol'] = "Br";
+elements[35]['name'] = "Bromine";
+elements[35]['weight'] = 79.904;
+
+elements[36] = {};
+elements[36]['number'] = 36;
+elements[36]['symbol'] = "Kr";
+elements[36]['name'] = "Krypton";
+elements[36]['weight'] = 83.798;
+
+elements[37] = {};
+elements[37]['number'] = 37;
+elements[37]['symbol'] = "Rb";
+elements[37]['name'] = "Rubidium";
+elements[37]['weight'] = 85.4678;
+
+elements[38] = {};
+elements[38]['number'] = 38;
+elements[38]['symbol'] = "Sr";
+elements[38]['name'] = "Strontium";
+elements[38]['weight'] = 87.62;
+
+
+elements[39] = {};
+elements[39]['number'] = 39;
+elements[39]['symbol'] = "Y";
+elements[39]['name'] = "Yttrium";
+elements[39]['weight'] = 88.905;
+
+elements[40] = {};
+elements[40]['number'] = 40;
+elements[40]['symbol'] = "Zr";
+elements[40]['name'] = "Zirconium";
+elements[40]['weight'] = 91.224;
+
+elements[41] = {};
+elements[41]['number'] = 41;
+elements[41]['symbol'] = "Nb";
+elements[41]['name'] = "Niobium";
+elements[41]['weight'] = 92.906;
+
+elements[42] = {};
+elements[42]['number'] = 42;
+elements[42]['symbol'] = "Mo";
+elements[42]['name'] = "Molybdenum";
+elements[42]['weight'] = 95.96;
+
+elements[43] = {};
+elements[43]['number'] = 43;
+elements[43]['symbol'] = "Tc";
+elements[43]['name'] = "Technetium";
+elements[43]['weight'] = 97.907;
+
+elements[44] = {};
+elements[44]['number'] = 44;
+elements[44]['symbol'] = "Ru";
+elements[44]['name'] = "Ruthenium";
+elements[44]['weight'] = 91.224;
+
+elements[45] = {};
+elements[45]['number'] = 45;
+elements[45]['symbol'] = "Rh";
+elements[45]['name'] = "Rhodium";
+elements[45]['weight'] = 102.90550;
+
+elements[46] = {};
+elements[46]['number'] = 46;
+elements[46]['symbol'] = "Pd";
+elements[46]['name'] = "Palladium";
+elements[46]['weight'] = 106.42;
+
+elements[47] = {};
+elements[47]['number'] = 47;
+elements[47]['symbol'] = "Ag";
+elements[47]['name'] = "Silver";
+elements[47]['weight'] = 107.868;
+
+elements[48] = {};
+elements[48]['number'] = 48;
+elements[48]['symbol'] = "Cd";
+elements[48]['name'] = "Cadmium";
+elements[48]['weight'] = 112.411;
+
+elements[49] = {};
+elements[49]['number'] = 49;
+elements[49]['symbol'] = "In";
+elements[49]['name'] = "Indium";
+elements[49]['weight'] = 114.818;
+
+elements[50] = {};
+elements[50]['number'] = 50;
+elements[50]['symbol'] = "Sn";
+elements[50]['name'] = "Tin";
+elements[50]['weight'] = 118.710;
+
+elements[51] = {};
+elements[51]['number'] = 51;
+elements[51]['symbol'] = "Sb";
+elements[51]['name'] = "Antimony";
+elements[51]['weight'] = 121.760;
+
+elements[52] = {};
+elements[52]['number'] = 52;
+elements[52]['symbol'] = "Te";
+elements[52]['name'] = "Tellurium";
+elements[52]['weight'] = 127.60;
+
+elements[53] = {};
+elements[53]['number'] = 53;
+elements[53]['symbol'] = "I";
+elements[53]['name'] = "Iodine";
+elements[53]['weight'] = 126.904;
+
+elements[54] = {};
+elements[54]['number'] = 54;
+elements[54]['symbol'] = "Xe";
+elements[54]['name'] = "Xenon";
+elements[54]['weight'] = 131.293;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+jQuery('.element').each(function(){
+  var number = $(this).attr('data-number');
+
+  $(this).find('.atomicNumber').html(elements[number].number)
+  $(this).find('.atomicWeight').html(elements[number].weight)
+  $(this).find('.atomicName').html(elements[number].name)
+  $(this).find('.atomicSymbol').html(elements[number].symbol)
+});
